a34869303b58c693f8e9ca9f561c1ae451420b7e
Jamie <31694@ma-web.nl>
2021-06-22 19:13:00 +0200

    Code + afb

---
+++ Code/script.js
@@ -0,0 +1,11 @@
+const menuBtn = document.querySelector('.burger-nav');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+  if(menuOpen) {
+    menuBtn.classList.add('open');
+  } else {
+    menuBtn.classList.remove('open');
+    menuOpen = false;
+  }
+});
+
